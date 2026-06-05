2dedbb440a7c8837c22cdd3237e5874f2f819325
Aarón <netaru3@gmail.com>
2026-06-05 17:17:54 -0300

    Update crear_tor.ts

--- Brute_force_ip_change/crear_tor.ts
+++ Brute_force_ip_change/crear_tor.ts
@@ -1,15 +1,27 @@
-//importaciones
+#!/usr/bin/env node
+
+
+
 import fs from 'fs'
 
+
+
 //declaración de variables
+
 let i=0
+
 let port=9052
 
+
+
 //codigo ejecutable
+
 while(i<=4){
+
     fs.writeFileSync(`/tmp/tor${i}rc`,`SOCKSPort ${port}
+
 ControlPort ${port +1}
+
 DataDirectory /tmp/tor${i}`); ++i; port=port+2
+
 }
-
-
