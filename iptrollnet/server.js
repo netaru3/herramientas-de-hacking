7e71283b653598bd35fa94c9927800678426ec95
Aarón <netaru3@gmail.com>
2026-06-25 16:48:45 -0300

    Add files via upload

---
+++ iptrollnet/server.js
@@ -0,0 +1,30 @@
+//imports
+import express from 'express'
+import {ip} from './database.js'
+import mongoose from 'mongoose'
+
+//declaraciones
+const app= express()
+
+
+//middeleware
+
+app.use(express.json())
+
+//rutas
+
+app.get("/",function(req,res){res.sendFile("iptrollnet.html",{root:import.meta.dirname})})
+
+app.post("/trollnet",async function(req,res){
+    console.log("la ip real es:",req.body.ip_real)
+
+    await ip.create({ip_real: req.body.ip_real, ip_registrada: req.ip})
+    
+    res.send("ok")
+
+
+})
+//escucha
+app.listen(3000,function(req,res){
+    console.log("server activado")
+})
